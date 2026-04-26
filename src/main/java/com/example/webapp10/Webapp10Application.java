e45e3e6a9460c26e8f2fc3f77d26163e39c927f6
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-26 18:00:24 +0600

    Init java project: src/main/java/com/example/webapp10/Webapp10Application.java

---
+++ src/main/java/com/example/webapp10/Webapp10Application.java
@@ -0,0 +1,13 @@
+package com.example.webapp10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp10Application.class, args);
+	}
+
+}
